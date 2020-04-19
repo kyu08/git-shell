89ac6e29f202c6b4ac4285007ea3ac7654a26039
Tatsuya Kyushima <kyu08@users.noreply.github.com>
2020-04-19 20:08:36 +0900

    ${commitMessage}

--- git-command.sh
+++ git-command.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 git add -p
 echo commit message たのむ
-read "Enter commit message." commitMessage
+read commitMessage
 git commit '-m${commitMessage}'
-read "Which branch?" branchToPush
+echo Which branch?
+read branchToPush
 if [ $branchToPush = 'master' ]; then
+    echo "本当に本当に master に push してええんか？ [y/n]"
     read -p "本当に本当に master に push してええんか？ [y/n]" answerAboutMaster
     if [ $answerAboutMaster = 'y' ]; then
         echo ほな $branchToPush に push するで〜
@@ -13,6 +15,7 @@ if [ $branchToPush = 'master' ]; then
         echo push しなかったで〜
     fi
 else
+    echo "${branchToPush} に push してええか？ [y/n]"
     read -p "${branchToPush} に push してええか？ [y/n]" answerAboutPush
     if [ $answerAboutPush = 'y' ]; then
         echo ほな $branchToPush に push するで〜
