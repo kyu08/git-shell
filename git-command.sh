c0463a76991bed7a5358abecb19edf09a6c3cfd0
Tatsuya Kyushima <kyu08@users.noreply.github.com>
2020-04-19 20:49:31 +0900

    スペースをいれた

--- git-command.sh
+++ git-command.sh
@@ -4,9 +4,9 @@ ESC=$(printf '\033')
 git add -p
 read -p "${ESC}[32mcommit message: ${ESC}[m" commitMessage
 git commit "-m${commitMessage}"
-read -p "${ESC}[32mWhich branch?${ESC}[m" branchToPush
+read -p "${ESC}[32mWhich branch? ${ESC}[m" branchToPush
 if [ $branchToPush = 'master' ]; then
-    read -p "${ESC}[32m本当に本当に master に push してええんか？ [y/n]${ESC}[m" answerAboutMaster
+    read -p "${ESC}[32m本当に本当に master に push してええんか？ [y/n] ${ESC}[m" answerAboutMaster
     if [ $answerAboutMaster = 'y' ]; then
         echo "${ESC}[32mほな $branchToPush に push するで〜${ESC}[m"
         git push origin $branchToPush
@@ -14,7 +14,7 @@ if [ $branchToPush = 'master' ]; then
         echo "${ESC}[32mpush しなかったで〜${ESC}[m"
     fi
 else
-    read -p "${ESC}[32m${branchToPush} に push してええか？ [y/n]${ESC}[m" answerAboutPush
+    read -p "${ESC}[32m${branchToPush} に push してええか？ [y/n] ${ESC}[m" answerAboutPush
     if [ $answerAboutPush = 'y' ]; then
         echo "${ESC}[32mほな $branchToPush に push するで〜${ESC}[m"
         git push origin $branchToPush
